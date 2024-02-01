b180a91564b2d8663dbaf506cd38f4254231c50a
web <chenanlun0418@gmail.com>
2024-02-01 10:54:31 +0800

    first commit

---
+++ prjshoppincar/prjshoppincar/Controllers/homeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using prjshoppincar.Models;
+
+namespace prjshoppincar.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        dbShoppingCarEntities db = new dbShoppingCarEntities();
+        public ActionResult Index()
+        {
+            var products = db.tProduct.ToList();  //傳給網頁顯示
+            if(Session["Member"]==null)
+            {
+                return View("Index", "_Layout", products);
+            }else
+            {
+                return View("Index", "_LayoutMember", products);
+            }
+            
+        }
+    }
+}
---
+++ prjshoppincar/prjshoppincar/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="prjshoppincar.MvcApplication" Language="C#" %>
